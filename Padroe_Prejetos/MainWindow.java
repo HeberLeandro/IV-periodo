270d67fde96738356cc840973a832092f76bfbeb
unknown <geanlucas0812@bol.com.br>
2020-03-04 21:35:50 -0300

    padrões

---
+++ Padroe_Prejetos/MainWindow.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+public class MainWindow extends JFrame {
+
+    MyPanel pCanvas;
+    JSpinner sQuantity;
+    
+    private void createWindow() {
+        this.setPreferredSize(new Dimension(618, 726));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JLabel titulo = new JLabel("Ordenação");
+        titulo.setFont(new Font("Serif", Font.BOLD, 24));
+        topPanel.add(titulo);
+        topPanel.setMaximumSize(new Dimension(640, 50));
+        this.add(topPanel);
+        
+        JPanel middlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        sQuantity = new JSpinner(new SpinnerNumberModel(25, 1, 2000, 1));
+        sQuantity.setToolTipText("Quantity of numbers to sort");
+        middlePanel.add(sQuantity);
+        JButton bShuffle = new JButton("Shuffle");
+        bShuffle.addActionListener(ae -> pCanvas.shuffle());
+        middlePanel.add(bShuffle);
+        middlePanel.setMaximumSize(new Dimension(640, 50));
+        this.add(middlePanel);
+
+        pCanvas = new MyPanel(this);
+        this.add(pCanvas);
+
+        this.pack();
+    }
+
+    public JSpinner getsQuantity() {
+        return sQuantity;
+    }
+    
+    public MainWindow() {
+        super("Sorting Algorithms");
+    }
+    
+    public static void main(String[] args) {
+        MainWindow w = new MainWindow();
+        w.createWindow();
+    }
+}
---
+++ Padroe_Prejetos/MyPanel.java
@@ -0,0 +1,50 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+
+    MainWindow frame;
+    protected List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+
+    public MyPanel(MainWindow mw) {
+        this.frame = mw;
+    }
+    
+    private void createRandomizedArray(int qtd) {
+        this.numbers = new ArrayList(qtd);
+        for(int i = 1; i <= qtd; i++) {
+            this.numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+    }
+    
+    public void shuffle() {
+        int quantity = (Integer) this.frame.getsQuantity().getValue();
+        this.createRandomizedArray(quantity);
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.clearRect(0, 0, this.getWidth(), this.getHeight());
+        g.setColor(Color.black);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+        int outerSpace = 5;
+        int innerSpace = 1;
+        int QTD = numbers.size();
+        int width = (this.getWidth() - 2 * outerSpace - (QTD - 1) * innerSpace) / QTD;
+        int height = (this.getHeight() - 2 * outerSpace) / QTD;
+        g.setColor(Color.white);
+        for (int idx = 0; idx < QTD; idx++) {
+            int n = numbers.get(idx);
+            g.fillRect(idx * (width + innerSpace) + outerSpace, this.getHeight() - n * height - outerSpace, width, n * height);
+        }
+    }
+}
