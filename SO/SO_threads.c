9656f2e42e7ad66a4c24c9b7c02ace6a26feb32e
heber leandro <heberleandroluz@gmail.com>
2020-09-23 21:30:00 -0300

    erro o juntar as threads

--- SO/SO_threads.c
+++ SO/SO_threads.c
@@ -7,16 +7,18 @@
 
 void *multArray(void * arr);
 
-typedef struct {
-	int idx, length;
-}thread_arg, *ptr_thread_arg;	
+typedef struct{
+	int *arr;
+	int start, final;
+}args;
+	
 
 int main (void){
 	
 	int array[ARRAY];
 	
-	//armazena o resultado quando as threads retornarem
-	int results [THREADS];
+	//armazena o resultado quando as threads retornarem, a threads retarnam um array de int
+	int *results[THREADS];
 	
 	pthread_t threads [THREADS];
 	
@@ -27,33 +29,42 @@ int main (void){
 	
 	//inicia as threads	
 	for(int i =0; i<THREADS; i++){
-		pthread_create(&threads[i], NULL, multArray,(void*)array, i*THREADS, ((i+1)*THREADS)-1);
+		args *a = (args*)malloc(sizeof(args));
+		a->arr = array;
+		a->start = i*THREADS;
+		a->final = ((i+1)*THREADS)-1;
+		//printf("start -> %d, final -> %d\n", a->start,a->final);
+		pthread_create(&threads[i], NULL, multArray,(void*)a);
 	}
 	
 	
 	//junta as threads dps da soma
 	for(int j =0; j<THREADS; j++){
 		pthread_join(threads[j], (void *)&results[j]);
+		printf("juntando thread %d\n",j);
 	}
 	
-	int mult=0;
+	
 	for(int l=0; l<THREADS; l++){
-		for(int i =0; i<ARRAY; i++){
-			printf("Multiplicação %d \n", results[l][i]);
-		}
+		//for(int i =0; i<THREADS; i++){
+			printf("Mult %i \n", *results[l]);
+		//}
 	}
 
 	return 0;
 }
 
-void *multArray(void *arr, int start, int final){
+void *multArray(void *arg){
 	
-	int mult[start-final];
-	
-	int *a = (int *) arr;
-	
-	for(int i =start; i< final; i++)mult[i] = a[i] + 2;
+	args *a = (args *) arg;
+
+	int mult[THREADS];	
+	for(int i = a->start, j = 0; i <= a->final; i++, j++){
+		mult[j] = a->arr[i] * 1;
+		//printf("mult %d\n",mult[j]);
+	}
+	printf("start %d terminei! \n", a->start);
 	 
-	return (void *)mult;
+	return (void *)&mult[0];
 }
 
